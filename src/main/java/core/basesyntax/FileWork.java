d73fcb7280150fbe6f710498113eb69c0ddfeeec
goffa-exe <n4v4rm0re666@gmail.com>
2026-08-16 23:13:32 +0200

    implement redFromFile method

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,42 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder builder = new StringBuilder();
+        
+        try {
+            Path path = Path.of(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path.toFile()));
+            String fileContent = bufferedReader.readLine();
+            
+            while (fileContent != null) {
+                String [] splitRow = fileContent.toLowerCase().split("[\\s.,?!]+");
+                for (String word : splitRow) {
+                    if (word.charAt(0) == 'w') {
+                        builder.append(word).append(" ");
+                    }
+                }
+                fileContent = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File was not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't open a file", e);
+        }
+        
+        if (builder.toString().isEmpty()) {
+            return new String[0];
+        }
+        String [] result = builder.toString().split(" ");
+        Arrays.sort(result);
+        
+        return result;
     }
 }
